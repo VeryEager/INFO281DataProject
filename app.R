099e01c59bdcc116635420c6c31d984cd5da124b
Asher Stout <stoutashe@myvuw.ac.nz>
2019-12-04 12:20:06 +1300

    Added more functionality to Shiny display

--- app.R
+++ app.R
@@ -23,84 +23,86 @@ inq_selection <- gsub("_", " ", inq_selection)
 
 # --------FORMAT UI--------
 ui <-
-  fluidPage(tabsetPanel(
-    #Country Analysis tab
-    tabPanel("Country Analysis", 
-      #Viewing and manipulating country analysis
-      sidebarLayout(
-        #Data selection for country/data analysis
-        sidebarPanel(
-          #Country select (and help button)
-          h4("Data Selection"),
-          
-          
-          inputPanel(
-            selectInput(
-              inputId = "country_select",
-              "Country",
-              choices = country_selection,
-              multiple = FALSE,
-              selectize = T
-            ),
-            actionButton(
-              inputId = "country_help",
-              label = "",
-              icon = helpicon
-            )
-          ),
-          
-          #Economic data select (and help button)
-          inputPanel(
-            selectInput(
-              inputId = "econ_select",
-              "Economic Indicator",
-              choices = econ_selection,
-              multiple = FALSE,
-              selectize = T
-            ),
-            actionButton(
-              inputId = "econ_help",
-              label = "",
-              icon = helpicon
-            )
+  fluidPage(
+    tabsetPanel(
+      #Country Analysis tab
+      tabPanel(
+        "Country Analysis",
+        #Viewing and manipulating country analysis
+        sidebarLayout(
+          #Data selection for country/data analysis
+          sidebarPanel(
+            #Country select (and help button)
+            h4("Data Selection"),
             
-          ),
-          
-          #Inequality data select (and help button)
-          inputPanel(
-            selectInput(
-              inputId = "inq_select",
-              "Inequality Indicator",
-              choices = inq_selection,
-              multiple = FALSE,
-              selectize = T
+            
+            inputPanel(
+              selectInput(
+                inputId = "country_select",
+                "Country",
+                choices = country_selection,
+                multiple = FALSE,
+                selectize = T
+              ),
+              actionButton(
+                inputId = "country_help",
+                label = "",
+                icon = helpicon
+              )
             ),
-            actionButton(
-              inputId = "inq_help",
-              label = "",
-              icon = helpicon
-            )
+            
+            #Economic data select (and help button)
+            inputPanel(
+              selectInput(
+                inputId = "econ_select",
+                "Economic Indicator",
+                choices = econ_selection,
+                multiple = FALSE,
+                selectize = T
+              ),
+              actionButton(
+                inputId = "econ_help",
+                label = "",
+                icon = helpicon
+              )
+              
+            ),
+            
+            #Inequality data select (and help button)
+            inputPanel(
+              selectInput(
+                inputId = "inq_select",
+                "Inequality Indicator",
+                choices = inq_selection,
+                multiple = FALSE,
+                selectize = T
+              ),
+              actionButton(
+                inputId = "inq_help",
+                label = "",
+                icon = helpicon
+              )
+            ),
+            
+            width = 3
           ),
           
-          width = 3
-        ),
-        
-        #Do analysis
-        
-        #view for country/data analysis
-        mainPanel(
-          plotOutput(outputId = "country_econ_plot"),
-          plotOutput(outputId = "country_inq_plot"),
-          verbatimTextOutput(outputId = "correlation_stats", placeholder = T)
-        ),
-        fluid = T
-      )
+          #Do analysis
+          
+          #view for country/data analysis
+          mainPanel(
+            plotOutput(outputId = "country_econ_plot"),
+            plotOutput(outputId = "country_inq_plot"),
+            verbatimTextOutput(outputId = "correlation_stats", placeholder = T)
+          ),
+          fluid = T
+        )
+      ),
+      tabPanel("New Zealand Analysis")
     ),
-    tabPanel("New Zealand Analysis")
-  ),
-  
-  title = "Trade Balance and Inequality; an Analysis",
-  theme = shinytheme("sandstone")
+    
+    title = "Trade Balance and Inequality; an Analysis",
+    theme = shinytheme("sandstone")
   )
 
 
@@ -112,8 +114,10 @@ server <-
     #render economic plot
     output$country_econ_plot <-
       renderPlot({
-        plot(econdata$Year,
-             econdata$Year,
+        plot_country <- econdata[, c("Country", gsub(" ", "_", input$econ_select))]
+        
+        plot(econdata$Year[1:10],
+             plot_country[plot_country$Country == input$country_select, 2],
              xlab = "Year",
              ylab = econdata$Units[1]) #TODO: econdata$Year needs to be changed to reflect appropriate row/col values
       })
@@ -121,10 +125,13 @@ server <-
     #render inequality plot
     output$country_inq_plot <-
       renderPlot({
-        plot(inqdata$Year,
-             inqdata$Year,
-             xlab = "Year",
-             ylab = "Index") #TODO: inqdata$Year and index need to be changed to reflect appropriate data
+        plot(
+          inqdata$Year,
+          inqdata$Year,
+          xlab = "Year",
+          ylab = "Index",
+          col = "#75AADB",
+        ) #TODO: inqdata$Year and index need to be changed to reflect appropriate data
       })
     
     #render statistical summary of country data
